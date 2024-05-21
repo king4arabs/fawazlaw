aa73ec3dcea738f8c97034f3ddb15e0b75c8e299
murtazaHw26 <145095363+murtazaHw26@users.noreply.github.com>
2024-05-21 17:30:06 +0400

    cart finalized

--- UI-WEB/frontend/src/components/CartItems.jsx
+++ UI-WEB/frontend/src/components/CartItems.jsx
@@ -34,7 +34,7 @@ const CartItems = () => {
                 key={idx}
                 className="relative w-full flex items-center flex-col-reverse sm:flex-row gap-[20px] px-[40px] sm:px-[50px] "
               >
-                <CartCard/>
+                <CartCard title={itm.name} price={itm.price}/>
               </ul>
             ))}
         </div>
@@ -43,13 +43,13 @@ const CartItems = () => {
 
 export default CartItems
 
-const CartCard = () => {
+const CartCard = ({ title, price }) => {
     return (
         <div className=" w-[90%] shadow-top-darkRed py-8 pr-10 border-2 rounded-lg flex flex-col items-end justify-end">
             <div className=" flex flex-col items-end gap-5 pt-4">
               <div className=" justify-end items-end flex flex-col gap-3 w-[full]">
                 <h1 className=" text-[20px] font-black text-end text-[#003E6F]">
-                  استشارات عقود العمل
+                  {title}
                 </h1>
                 <div className="w-full subsection flex gap-2">
                   <h3 className="text-[18px] text-end max-w-sm mx-auto break-words text-[#667085] font-medium pr-3">
@@ -66,7 +66,7 @@ const CartCard = () => {
             <div className="justify-end items-center flex content-center w-full transition duration-[100ms] gap-1 pt-5">
               <span className="text-[18px] text-[#081F2F] font-bold">سنويا</span>
               <span className="text-[18px] text-[#667085] font-black">SAR</span>
-              <span className="text-[23px] text-[#081F2F] font-black">1200</span>
+              <span className="text-[23px] text-[#081F2F] font-black">{price}</span>
             </div>
             <button className="pt-4">
             <p className="border border-[#F04438] text-[#F04438] font-bold text-[18 px] rounded-[8px] px-[45px] py-[16px]">حذف</p>
